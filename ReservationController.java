0a2d6eae1795cc0fbf8670bf540ddc69404dcbb0
linda59 <mchareklinda@hotmail.fr>
2023-01-03 12:39:51 +0100

    Add files via upload
    Add controller for role and reservation

---
+++ ReservationController.java
@@ -0,0 +1,60 @@
+package com.project.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.dao.ReservationDao;
+import com.project.modele.Reservation;
+import com.project.modele.Role;
+
+@RestController
+@RequestMapping("airBnB/reservation")
+public class ReservationController {
+	@Autowired
+	private ReservationDao reservationDao;
+	
+	
+	// Get List of Reservation
+	@GetMapping("")
+	public List<Reservation> findAllReservations() {
+		return this.reservationDao.findAll();
+	}
+	
+	// Get One  Reservation by id
+	@GetMapping("/{id}")
+	public Optional<Reservation> findOneResById(@PathVariable int id) {
+		return this.reservationDao.findById(id);
+	}
+	
+	// Delete Reservation
+	@DeleteMapping("/delete/{id}")
+	public Boolean deleteRes(@PathVariable int id) {
+		this.reservationDao.deleteById(id);
+		return true;
+	}
+	
+	// Insert Reservation
+	@PostMapping("/add")
+	@ResponseBody
+	public Reservation insertRes( @RequestBody Reservation res  ) {
+		return this.reservationDao.save(res);
+	}
+	
+	// Update Reservation
+	@PutMapping("/update")
+	public Reservation updateRole( @RequestBody Reservation res ) {
+		return this.reservationDao.save(res);
+	}
+
+}
---
+++ UsersController.java
@@ -0,0 +1,57 @@
+package com.project.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.dao.UsersDao;
+import com.project.modele.Users;
+
+@RestController
+@RequestMapping("airBnB/users")
+public class UsersController {
+	@Autowired
+	private UsersDao usersDao;
+
+	// Get List of users
+	@GetMapping("/users")
+	public List<Users> findAllUsers() {
+		return this.usersDao.findAll();
+	}
+	
+	// Get One  User by id
+	@GetMapping("/{id}")
+	public Optional<Users> findOneUserById(@PathVariable int id) {
+		return this.usersDao.findById(id);
+	}
+	
+	// Delete User
+	@DeleteMapping("/delete/{id}")
+	public Boolean deleteUser(@PathVariable int id) {
+		this.usersDao.deleteById(id);
+		return true;
+	}
+	
+	// Insert User
+	@PostMapping("/add")
+	public Users insertUser( @RequestBody Users users  ) {
+		return this.usersDao.save(users);
+	}
+	
+	// Update user
+		@PutMapping("/update")
+		public Users updateUser( @RequestBody Users users  ) {
+			return this.usersDao.save(users);
+		}
+
+
+}
